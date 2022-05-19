fb7236912f6fd4fd4fdfb84a4a7bf139a8de8198
IanMcbull <ianmugenya@gmail.com>
2022-05-19 21:18:04 +0300

    Added parameter functionality

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', async(req, res)=>{
+    console.log(req.query)
     try{
-    const response = await axios(`https://api.spoonacular.com/recipes/search?apiKey=${process.env.SPOONACULAR_API_KEY}&number=${req.query.number||5}`)
-    res.send(response.data);
+    const response = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_API_KEY}&number=${req.query.number||5}&cuisine=${req.query.cuisine||''}&diet=${req.query.diet||''}&author=${req.query.author||''}&query=${req.query.query||''}`)
+    res.json(response.data);
  }catch(err){
    res.send(JSON.stringify(err));
  }
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const port = process.env.PORT || 3300;
 const morgan = require('morgan');
 const colors = require('colors');
-const router = require('./routes/recipes')
+const recipes = require('./routes/recipes')
 require('dotenv').config();
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 // Testing the get request
-app.use(router)
+app.use(recipes)
 
 app.listen(port,()=>{
     console.log(`listening on port: ${colors.yellow(port)}`);
